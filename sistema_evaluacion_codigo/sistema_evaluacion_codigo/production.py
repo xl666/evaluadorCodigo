ed54362dae05bc145ecf3c290d720fbec4d01b47
xl666 <xavi004@gmail.com>
2019-02-10 00:44:20 -0600

    -Se agrega servicio de nginx con manejo de contenido estático

--- sistema_evaluacion_codigo/sistema_evaluacion_codigo/production.py
+++ sistema_evaluacion_codigo/sistema_evaluacion_codigo/production.py
@@ -7,7 +7,7 @@ env_file = os.path.join(BASE_DIR, '.env')
 environ.Env.read_env(env_file)
 
 SECRET_KEY = env("SECRET_KEY_PRODUCTION")
-DEBUG = True
+DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
