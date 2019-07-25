0ea4224e42fe4ba4dc061f4eb3b27585256df191
Xavier <xavi004@gmail.com>
2019-07-25 11:51:27 -0500

    -Se refactorizó el html para separar css y js

---
+++ sandbox/sandboxer/app/static/js/all.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $("#irEvaluador").click(function() {
+	$("#consola").hide();
+	$("#evaluador").show();
+    });
+
+    $("#irConsola").click(function() {
+	$("#consola").show();
+	$("#evaluador").hide();
+    });
+
+});
