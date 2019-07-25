931bc626dabcbf97f8feb482482f57e6616db212
Xavier <xavi004@gmail.com>
2019-07-25 18:42:57 -0500

    -Se propaga el token al contenedor

--- sandbox/monitor/monitor.py
+++ sandbox/monitor/monitor.py
@@ -11,6 +11,8 @@ import os
 import random
 
 IpPuerto = namedtuple('IpPuerto', 'ip puerto')
+SIMBOLOS = [chr(c) for c in range(65, 91)] + [chr(c) for c in range(97, 123)] + [str(i) for i in range(10)]
+
 
 def notificar(data):
     mensaje = '%s: %s' % (datetime.datetime.now(), data)
@@ -38,22 +40,39 @@ def get_puerto_libre():
         if not puerto in usados:
             return puerto
 
-def lanzar_contenedor(ip, ipsPuertos):
-    comando = 'docker run --rm -d -p %s:7681 cplus2ttyd'
+def lanzar_contenedor(ip, ipsPuertos, token):
+    if not token:
+        raise Exception('No se puede iniciar el contenedor sin un token')
+    comando = 'docker run --rm -d -p %s:7681 -e TOKEN=%s cplus2ttyd'
     ipPuerto = ipInIpsPuertos(ip, ipsPuertos)
     if ipPuerto:
         return ipPuerto
     puerto = get_puerto_libre()
     print('Lanzando contenedor en puerto %s' % puerto)
-    os.system(comando % puerto)
+    os.system(comando % (puerto, token))
     return IpPuerto(ip, puerto)
+
+def gen_token(longitud=10):
+    res = ''
+    for i in range(longitud):
+        res += random.choice(SIMBOLOS)
+    return res
+
+def get_token(ip, dictTokens):
+    if ip in dictTokens.keys():
+        return dictTokens[ip]
+    while True:
+        token = gen_token()
+        if not token in dictTokens.values():
+            return token
+    assert False # imposible
 
 class Monitor():
     def __init__(self, port=9030):        
         self.port = port
         self.lock = threading.Lock()
         self.ipsPuertos = set()
-        
+        self.dictTokens = {}
         
     def run(self):
         """
@@ -65,40 +84,67 @@ class Monitor():
         mySocket.listen(5)
         while True:
             conn, _ = mySocket.accept()
-            attendThread = WorkThread(conn, self.lock, self.ipsPuertos) # se crea un hilo de atención por cliente
+            attendThread = WorkThread(conn, self.lock, self.ipsPuertos, self.dictTokens) # se crea un hilo de atención por cliente
             attendThread.start()
 
             
 class WorkThread(threading.Thread):  # it is actually a subprocess
-    def __init__(self, conn, lock, ipsPuertos):
+    def __init__(self, conn, lock, ipsPuertos, dictTokens):
         self.conn = conn
         self.lock = lock
         self.ipsPuertos = ipsPuertos
-        self.eventos = {'notificar': notificar, 'lanzar': lanzar_contenedor}
+        self.dictTokens = dictTokens
+        self.eventos = {'notificar': notificar, 'lanzar': lanzar_contenedor,
+                        'darToken': get_token}
         threading.Thread.__init__(self)
 
+    def notificar(self, data):
+        data = data[10:]
+        self.eventos['notificar'](data)
+
+    def lanzar(self, data):
+        data = data[7:]
+        self.lock.acquire()
+        try:
+            ipPuerto = self.eventos['lanzar'](data, self.ipsPuertos, self.dictTokens.get(data.decode('utf-8'), None))
+            self.ipsPuertos.add(ipPuerto)
+        except:
+            raise
+        finally:
+            self.lock.release()
+        self.conn.send(str(ipPuerto.puerto).encode('utf-8'))
+
+    def darToken(self, data):
+        data = data[9:]
+        self.lock.acquire()
+        token = self.eventos['darToken'](data.decode('utf-8'), self.dictTokens)
+        self.dictTokens[data.decode('utf-8')] = token
+        self.lock.release()
+        self.conn.send(token.encode('utf-8'))
+        
     def run(self):
         data = b''
-        while not data.endswith(b'$$$'): # read message
-            chunck = self.conn.recv(1024)
-            data += chunck
-            if not chunck: #finished
-                break
-        if not data:
-            raise RuntimeError("No se transmitieron datos")
-        data = data[:-3] # quitar $$$
-        
-        if data.startswith(b'notificar:'):
-            data = data[10:]
-            self.eventos['notificar'](data)
+        try:
+            while not data.endswith(b'$$$'): # read message
+                chunck = self.conn.recv(1024)
+                data += chunck
+                if not chunck: #finished
+                    break
+            if not data:
+                raise RuntimeError("No se transmitieron datos")
+            data = data[:-3] # quitar $$$
+            
+            if data.startswith(b'notificar:'):
+                self.notificar(data)
+            if data.startswith(b'lanzar:'):
+                self.lanzar(data)
 
-        if data.startswith(b'lanzar:'):
-            data = data[7:]
-            ipPuerto = self.eventos['lanzar'](data, self.ipsPuertos)
-            self.lock.acquire()
-            self.ipsPuertos.add(ipPuerto)
-            self.lock.release()
-            self.conn.send(str(ipPuerto.puerto).encode('utf-8'))
+            if data.startswith(b'darToken:'):
+                self.darToken(data)
+        except:
+            raise
+        finally:
+            self.conn.close()
 
 if __name__ == '__main__':
     dem = Monitor()
--- sandbox/sandboxer/app/sandboxer/views.py
+++ sandbox/sandboxer/app/sandboxer/views.py
@@ -15,19 +15,48 @@ def get_client_ip(request):
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
+def recuperarToken(request, ip):
+    token = request.session.get('token', None)
+    if not token:
+        try:
+            cliente = socket.socket()
+            cliente.connect(('localhost', int(settings.PUERTO_MONITOR)))
+            cliente.send(b'darToken:%s%s' % (ip.encode('utf-8'), DELIMITADOR))
+            token = cliente.recv(1024)
+            token = token.decode('utf-8')
+            request.session['token'] = token
+            cliente.close()
+        except:
+            raise
+        finally:
+            cliente.close()
+    return token
+
+def lanzarContenedor(request, token, ip):
+    puerto = request.session.get('puerto', None)
+    if not puerto:
+        try:
+            cliente = socket.socket()
+            cliente.connect(('localhost', int(settings.PUERTO_MONITOR)))
+            cliente.send(b'lanzar:%s%s' % (ip.encode('utf-8'), DELIMITADOR))
+            puerto = int(cliente.recv(1024))
+        except:
+            raise
+        finally:
+            cliente.close()
+    return puerto
+
 def index(request):
     ip = get_client_ip(request)
-    cliente = socket.socket()
     try:
-        cliente.connect(('localhost', int(settings.PUERTO_MONITOR)))
-        cliente.send(b'lanzar:%s%s' % (ip.encode('utf-8'), DELIMITADOR))
-        puerto = int(cliente.recv(1024))
-        print(puerto)
+        token = recuperarToken(request, ip)
+        print(token)
+        puerto = lanzarContenedor(request, token, ip)
+        request.session['token'] = token
+        request.session['puerto'] = puerto        
+        
         return render_to_response('index.html', {'urlEvaluador': settings.EVALUADOR_URL, 'puerto': puerto})
     except Exception as err:
-        print('CAEEEEEEEEEEEEEEEEEEEEEe')
         print(err)
-    finally:
-        cliente.close()
     
     
