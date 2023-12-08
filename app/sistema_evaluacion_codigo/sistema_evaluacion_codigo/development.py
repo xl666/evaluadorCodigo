d07ef05baaa105737d155a90889bce2a8250b1aa
Daniel Díaz <danielnochess@gmail.com>
2023-12-08 10:48:47 -0600

    Filtrado por periodo

--- app/sistema_evaluacion_codigo/sistema_evaluacion_codigo/development.py
+++ app/sistema_evaluacion_codigo/sistema_evaluacion_codigo/development.py
@@ -1,5 +1,5 @@
 import environ
-
+import os
 from sistema_evaluacion_codigo.base import *
 
 env = environ.Env()
@@ -8,12 +8,18 @@ environ.Env.read_env(env_file)
 
 SECRET_KEY = env("SECRET_KEY_DEVELOPMENT")
 DEBUG = True
+SESSION_COOKIE_HTTPONLY = True
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME', 'evalcod'),
+        'USER': os.environ.get('DATABASE_USER', 'Buzz'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'TingaDePollo953'),
+        'HOST': os.environ.get('DATABASE_HOST', 'bd'),
+        'PORT': os.environ.get('DATABASE_PORT', '3306'),
     }
 }
+
--- app/sistema_evaluacion_codigo/sistema_evaluacion_codigo/production.py
+++ app/sistema_evaluacion_codigo/sistema_evaluacion_codigo/production.py
@@ -7,12 +7,13 @@ env_file = os.path.join(BASE_DIR, '.env')
 environ.Env.read_env(env_file)
 
 SECRET_KEY = env("SECRET_KEY_PRODUCTION")
-DEBUG = False
+DEBUG = True
+CSRF_COOKIE_HTTPONLY = True
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
-    'default': {
+    'default': {    
         'ENGINE': 'django.db.backends.mysql',
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
